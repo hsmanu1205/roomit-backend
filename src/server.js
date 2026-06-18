08f84e4b1ab940a25708f17200e333049f52e6f9
Harshit Singh <harshit.singh.hs9454@gmail.com>
2026-06-19 01:50:35 +0530

    added vercel url

--- src/server.js
+++ src/server.js
@@ -13,7 +13,15 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://roomit-frontend-alpha.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
